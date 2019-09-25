c7a3c2699454ea50ecba84e43d67a85826c374c9
BuildTools <unconfigured@null.spigotmc.org>
2019-09-25 20:59:29 +0900

    버튼 + 4개화면 추가 ( GPS 등산 나들이 근처식당 )

--- app/src/main/java/com/example/gongmo/MainActivity.java
+++ app/src/main/java/com/example/gongmo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gongmo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void GPSbutton(View v){
+        Intent intent = new Intent(this, GPSactivity.class);
+        startActivity(intent);
+    }
+
+    public void Climbbutton(View v){
+        Intent intent = new Intent(this, Climbactivity.class);
+        startActivity(intent);
+    }
+
+    public void Picnicbutton(View v){
+        Intent intent = new Intent(this, Picnicactivity.class);
+        startActivity(intent);
+    }
+
+    public void Restaurantbutton(View v){
+        Intent intent = new Intent(this, Restaurantactivity.class);
+        startActivity(intent);
     }
 }
