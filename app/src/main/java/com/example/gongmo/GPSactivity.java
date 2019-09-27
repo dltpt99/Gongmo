99664b385577492fada30097eb45b056f8a5a682
igkrap <parkgi45@gmail.com>
2019-09-27 21:02:40 +0900

    gps api 호출 및 현재위치

--- app/src/main/java/com/example/gongmo/GPSactivity.java
+++ app/src/main/java/com/example/gongmo/GPSactivity.java
@@ -35,64 +35,9 @@ public class GPSactivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-/*
-
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            return;
-        }
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
-                100, // 통지사이의 최소 시간간격 (miliSecond)
-                1, // 통지사이의 최소 변경거리 (m)
-                mLocationListener);
-        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
-                100, // 통지사이의 최소 시간간격 (miliSecond)
-                1, // 통지사이의 최소 변경거리 (m)
-                mLocationListener);
-
     }
 
-    private final LocationListener mLocationListener = new LocationListener() {
-        GPSactivity gps=new GPSactivity();
-        public void onLocationChanged(Location location) {
-            //여기서 위치값이 갱신되면 이벤트가 발생한다.
-            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
 
-
-//Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
-                gps.longitude = location.getLongitude();    //경도
-                gps.latitude = location.getLatitude();         //위도
-
-
-
-
-
-        }
-        public void onProviderDisabled(String provider) {
-            // Disabled시
-
-        }
-
-        public void onProviderEnabled(String provider) {
-            // Enabled시
-
-        }
-
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            // 변경시
-
-        }
-
-    };
-
-*/
 
         /**
          * Manipulates the map once available.
@@ -103,16 +48,15 @@ public class GPSactivity extends FragmentActivity implements OnMapReadyCallback 
          * it inside the SupportMapFragment. This method will only be triggered once the user has
          * installed Google Play services and returned to the app.
          */
-    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         GPSactivity gps=new GPSactivity();
-        int lat=(int)gps.latitude;
-        int lng=(int)gps.longitude;
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(32,31 );
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng current = new LatLng(gps.latitude,gps.longitude );
+        mMap.addMarker(new MarkerOptions().position(current ).title("현재 위치"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current ));
     }
 }
--- app/src/main/java/com/example/gongmo/MainActivity.java
+++ app/src/main/java/com/example/gongmo/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.gongmo;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,10 +23,54 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void GPSbutton(View v){
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void GPSbutton(View v) {
         Intent intent = new Intent(this, GPSactivity.class);
         startActivity(intent);
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                100, // 통지사이의 최소 시간간격 (miliSecond)
+                1, // 통지사이의 최소 변경거리 (m)
+                mLocationListener);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                100, // 통지사이의 최소 시간간격 (miliSecond)
+                1, // 통지사이의 최소 변경거리 (m)
+                mLocationListener);
     }
+    private final LocationListener mLocationListener = new LocationListener() {
+        GPSactivity gps=new GPSactivity();
+        public void onLocationChanged(Location location) {
+            //여기서 위치값이 갱신되면 이벤트가 발생한다.
+            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
+
+
+//Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
+            gps.longitude = location.getLongitude();    //경도
+            gps.latitude = location.getLatitude();         //위도
+        }
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+        }
+    };
 
     public void Climbbutton(View v){
         Intent intent = new Intent(this, Climbactivity.class);
