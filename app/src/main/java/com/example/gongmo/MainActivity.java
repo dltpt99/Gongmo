e456e2c9cc60ef952dccb0d60aac10a8f02a6f8b
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 23:32:53 +0900

    Mainactivity 리뉴얼 ( 버튼을 이미지버튼으로 바꿈 )

--- app/src/main/java/com/example/gongmo/MainActivity.java
+++ app/src/main/java/com/example/gongmo/MainActivity.java
@@ -3,20 +3,22 @@ package com.example.gongmo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     ViewFlipper v_fllipper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int images[] = {
-                R.drawable.ic_launcher_background,
-                R.drawable.ic_launcher_foreground,
-                R.drawable.splash
+        int[] images = {
+                R.drawable.mt_bukhan,
+                R.drawable.mt_chunggye,
+                R.drawable.mt_dobong
         };
         v_fllipper = findViewById(R.id.image_slide);
 
@@ -24,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
             fllipperImages(image);
         }
 
-
     }
+
     public void fllipperImages(int image) {
         ImageView imageView = new ImageView(this);
         imageView.setBackgroundResource(image);
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         v_fllipper.setInAnimation(this,android.R.anim.slide_in_left);
         v_fllipper.setOutAnimation(this,android.R.anim.slide_out_right);
     }
+
 
 
     public void GPSbutton(View v) {
