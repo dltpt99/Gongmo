a7bf04987111020952f83d3d0c58d078561203be
BuildTools <unconfigured@null.spigotmc.org>
2019-09-29 13:50:53 +0900

    MAIN_ACTIVITY 이미지, 버튼 교체 ( 상준이형껄로 )

--- app/src/main/java/com/example/gongmo/Climbactivity.java
+++ app/src/main/java/com/example/gongmo/Climbactivity.java
@@ -2,7 +2,9 @@ package com.example.gongmo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Climbactivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class Climbactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climbactivity);
+
+    }
+
+    public void climb_easy(View v){
+        Intent intent = new Intent(this, Climbeasy.class);
+        startActivity(intent);
+    }
+
+    public void climb_normal(View v){
+        Intent intent = new Intent(this, Climbnormal.class);
+        startActivity(intent);
+    }
+
+    public void climb_hard(View v){
+        Intent intent = new Intent(this, Climbhard.class);
+        startActivity(intent);
     }
 }
