3f7a62b69c9293d9ace4bc5f148b79bef91d1d0a
igkrap <parkgi45@gmail.com>
2019-09-29 13:17:58 +0900

    뷰페이저 대신 이미지슬라이드로 꾸며봄 + splash 이미지 첨부

--- app/src/main/java/com/example/gongmo/Climbactivity.java
+++ app/src/main/java/com/example/gongmo/Climbactivity.java
@@ -2,7 +2,9 @@ package com.example.gongmo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Climbactivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Climbactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climbactivity);
+    }
+    public void beginner(View v){
+        Intent intent = new Intent(this, beginner.class);
+        startActivity(intent);
+    }
+    public void ama(View v){
+        Intent intent = new Intent(this, ama.class);
+        startActivity(intent);
+    }
+    public void pro(View v){
+        Intent intent = new Intent(this, pro.class);
+        startActivity(intent);
     }
 }
